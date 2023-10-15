6ffa3733f98cec44749f8422de708eb63bb63429
Danil Egorkin <danilegorkin@Danils-Air.himki.net>
2023-10-15 20:06:59 +0300

    auth

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+import psycopg2
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+conn = psycopg2.connect(database="kisprod",  
+                        user="postgres", 
+                        password="",  
+                        host="localhost", 
+                        port="5432")
 
 @app.route('/')
+@app.route('/authorization', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM adminBD') 
+        user = cur.fetchone()
+        if user[1]==username and user[2]==password:
+            return "SO good`s"
+    return render_template('formauth.html')
+    
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=83)
