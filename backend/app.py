eef3941835e65948f8ffe41937534d4afb501c7a
Danil Egorkin <danilegorkin@Danils-Air.himki.net>
2023-10-22 19:09:46 +0300

    update file

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 import psycopg2
 
 
@@ -11,18 +11,33 @@ conn = psycopg2.connect(database="kisprod",
                         host="localhost", 
                         port="5432")
 
+cursor = conn.cursor()
+
 @app.route('/')
-@app.route('/authorization', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM adminBD') 
-        user = cur.fetchone()
-        if user[1]==username and user[2]==password:
-            return "SO good`s"
+@app.route('/login')
+def login():
     return render_template('formauth.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        cursor.execute(('''select password from adminbd where user_name='{}';''').format(username))
+        passwords = cursor.fetchall()
+        if passwords[0][0] == password:
+            return redirect(url_for('mainpage'))
+        else:
+            return redirect(url_for('login'))
+
+@app.route('/mainpage')
+def mainpage():
+    return render_template('main_page.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
     
 
 app.run(host='0.0.0.0', port=83)
