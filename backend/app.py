d3da9f19f8892a91c0ba134af1820cc619d4af60
elmore17 <danilegdorkin@gmail.com>
2024-02-09 23:12:34 +0300

    global update

--- backend/app.py
+++ backend/app.py
@@ -20,6 +20,18 @@ def get_db_connection():
 def authorization():
     return render_template('Authorization.html')
 
+@app.route('/registration')
+def registration():
+    return render_template('Registration.html')
+
+@app.route('/gecpage')
+def gecpage():
+    return render_template('GecPage.html')
+
+@app.route('/commissionpage')
+def commissionpage():
+    return render_template('CommissionPage.html')
+
 @app.route('/login', methods=['POST'])
 def login_post():
     if request.method == 'POST':
@@ -34,7 +46,7 @@ def login_post():
             if user_data:
                 hashed_password = user_data[0]
                 if bcrypt.check_password_hash(hashed_password, password):
-                    return redirect(url_for('registration'))
+                    return redirect(url_for('gecpage'))
                 else:
                     flash('Неверный логин или пароль', "info")
                     return redirect(url_for('authorization'))
@@ -42,35 +54,103 @@ def login_post():
                 flash('Этот пользователь не найден', "info")
                 return redirect(url_for('authorization'))
         
-@app.route('/registration')
-def registration():
-    return render_template('Registration.html')
 
 @app.route('/addadmin', methods=['POST'])
 def add_admin():
     if request.method == 'POST':
         username = request.form.get('login')
         password = request.form.get('password1')
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        
-        #Проверка наличия пользователя
-        with get_db_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM adminbd WHERE user_name = %s", (username,))
-            existing_user = cursor.fetchone()
+        password2 = request.form.get('password2')
+        if password == password2:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            
+            #Проверка наличия пользователя
+            with get_db_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM adminbd WHERE user_name = %s", (username,))
+                existing_user = cursor.fetchone()
 
-            #Обработка ошибки о наличии пользователя
-            if existing_user:
-                flash('Такой пользователь уже существует', "info")
-                return redirect(url_for('registration'))
+                #Обработка ошибки о наличии пользователя
+                if existing_user:
+                    flash('Такой пользователь уже существует', "info")
+                    return redirect(url_for('registration'))
+            
+            #Создание нового пользователя
+            with get_db_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO adminbd (user_name, password) VALUES (%s, %s)", (username, hashed_password))
+                conn.commit()
+                return redirect(url_for('authorization'))
+        else:
+            flash('Пароли не совпадают', "info")
+            return redirect(url_for('registration'))
         
-        #Создание нового пользователя
-        with get_db_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO adminbd (user_name, password) VALUES (%s, %s)", (username, hashed_password))
-            conn.commit()
-            return redirect(url_for('authorization'))
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    try:
+        uploaded_file = request.files['file']
+        downloads_directory = Path.home() / 'Downloads'
+        file_path = downloads_directory / uploaded_file.filename
+        uploaded_file.save(file_path)
+        output_json = 'output.json'
+        data_text = read_docx(file_path)
+        create_json(data_text, output_json)
+        return {'status': 'success', 'message': 'File uploaded successfully', 'filePath': str(file_path)}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
 
+   
+@app.route('/getjsoninfo', methods=['GET', 'POST'])
+def get_json_info():
+    json_file_path = 'output.json'
+    if request.method == 'GET':
+        with open(json_file_path, 'r', encoding='utf-8') as json_file:
+            all_data = json.load(json_file)
+
+            # Создаем список студентов
+            students = []
+            for entry in all_data:
+                student_name = entry['student']
+                score = entry['score']
+                scoredip = entry['scoredip']
+
+                # Создаем объект для каждого студента
+                student = {'name': student_name, 'score': score, 'scoredip': scoredip}
+                students.append(student)
+
+        # Возвращаем список студентов в формате JSON
+        return jsonify({'students': students})
+    if request.method == 'POST':
+        student = request.form.get('student')
+        score = request.form.get('score')
+        scoredip = request.form.get('scoredip')
+        with open(json_file_path, 'r', encoding='utf-8') as json_file:
+            all_data = json.load(json_file)
+            for entry in all_data:
+                if entry['student'] == student:
+                    entry['score'] = score
+                    entry['scoredip'] = scoredip
+        with open(json_file_path, 'w', encoding='utf-8') as json_file:
+            json.dump(all_data, json_file, ensure_ascii=False, indent=4)
+        return jsonify({'status': 'success'})
+
+@app.route('/getusers', methods=['GET'])
+def get_users():
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users_commission')
+        users = cursor.fetchall()
+        user_list = []
+        for user in users:
+            user_dict = {
+                'id': user[0],
+                'user_name': user[1],
+                'post': user[2],
+                'data_start': user[3],
+                'data_end': user[4]
+            }
+            user_list.append(user_dict)
+        return jsonify({'users': user_list})
         
 # @app.route('/addusers', methods=['POST'])
 # def add_users():
@@ -85,61 +165,7 @@ def add_admin():
 #         conn.commit()
 #         return jsonify({'message': 'Successfully'})
     
-# @app.route('/getusers', methods=['GET'])
-# def get_users():
-#     cursor.execute('SELECT * FROM users')
-#     users = cursor.fetchall()
-#     user_list = []
-#     for user in users:
-#         user_dict = {
-#             'id': user[0],
-#             'user_name': user[1],
-#             'post': user[2]
-#         }
-#         user_list.append(user_dict)
-#     return jsonify({'users': user_list})
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     try:
-#         uploaded_file = request.files['file']
-#         downloads_directory = Path.home() / 'Downloads'
-#         file_path = downloads_directory / uploaded_file.filename
-#         uploaded_file.save(file_path)
-#         output_json = 'output.json'
-#         data_text = read_docx(file_path)
-#         create_json(data_text, output_json)
-#         return {'status': 'success', 'message': 'File uploaded successfully'}
-#     except Exception as e:
-#         return {'status': 'error', 'message': str(e)}
-    
-# @app.route('/getjsoninfo', methods=['GET', 'POST'])
-# def get_json_info():
-#     json_file_path = 'output.json'
-#     if request.method == 'GET':
-#         data_by_student = {}
-#         with open(json_file_path, 'r', encoding='utf-8') as json_file:
-#             all_data = json.load(json_file)
-#             for entry in all_data:
-#                 student_name = entry['student']
-#                 score = entry['score']
-#                 scoredip = entry['scoredip']
-#                 if student_name not in data_by_student:
-#                     data_by_student[student_name] = {'score': score, 'scoredip': scoredip, 'name': student_name}
-#         return jsonify({'users': data_by_student})
-#     if request.method == 'POST':
-#         student = request.json.get('student')
-#         score = request.json.get('score')
-#         scoredip = request.json.get('scoredip')
-#         with open(json_file_path, 'r', encoding='utf-8') as json_file:
-#             all_data = json.load(json_file)
-#             for entry in all_data:
-#                 if entry['student'] == student:
-#                     entry['score'] = score
-#                     entry['scoredip'] = scoredip
-#         with open(json_file_path, 'w', encoding='utf-8') as json_file:
-#             json.dump(all_data, json_file, ensure_ascii=False, indent=4)
-#         return jsonify({'message': 'Successfully'})
     
 
 app.run(host='0.0.0.0', port=83)
